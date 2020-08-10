bdaa8ce2d9fcf3c04dc00fcf9754206e48b92550
ArielHolman <ArielARodrigues01@gmail.com>
2020-08-09 22:05:03 -0500

    added all functionality

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
 /* eslint-disable max-classes-per-file */
-window.onload = function () {
-  listApplicants(arrOfApplicants)
-}
-
-// {
-//   id: 2,
-//   name: "Charles Young",
-//   age: 55,
-//   skillSet: "welding",
-//   placeBorn: "Omaha, Nebraska",
-// },
 let arrOfApplicants = [
   {
     id: 2,
@@ -62,57 +51,156 @@ let arrOfApplicants = [
   },
 ]
 
+// id: 2,
+//     name: "Charles Young",
+//     age: 55,
+//     skillSet: "welding",
+//     placeBorn: "Omaha, Nebraska",
+
 const listOfPlayers = []
 const dodgeFathersTeam = []
 const ballarinasTeam = []
 
-/* <button onclick="submit()">Submit</button> */
+class Applicant {
+  constructor(name, id, age, skillSet, placeBorn) {
+    this.id = id
+    this.age = age
+    this.name = name
+    this.skillSet = skillSet
+    this.placeBorn = placeBorn
+  }
+}
 
-// const submit = () => {
-//   newApplicant = ""
-//   arrOfApplicants.push(newApplicant)
-// }
-
-class Player {
-  constructor(canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience) {
+const makeTeamMate = (teamName, id, name) => {
+  // if onclick dodge fathers then assign mascot, color and move to dodgefathers array and show on the DOM
+  // if onclick ballerinas then assign mascot, color and move to ballerinas array and show on the DOM
+  if (teamName === "dodgeFathers") {
+    const teamMate = new TeamMate(
+      name,
+      "Guns and Roses",
+      "Blue",
+      id,
+      teamName
+    ).addTeamMate()
+    dodgeFathersTeam.push(teamMate)
+  } else {
+    const teamMate = new TeamMate(
+      name,
+      "Swans",
+      "Red",
+      id,
+      teamName
+    ).addTeamMate()
+    ballarinasTeam.push(teamMate)
+  }
+}
+// Add player class to people who become players
+class Player extends Applicant {
+  constructor(
+    name,
+    id,
+    canThrowBall,
+    canDodgeBall,
+    hasPaid,
+    isHealthy,
+    yearsExperience
+  ) {
+    super(name, id)
     this.canThrowBall = canThrowBall
     this.canDodgeBall = canDodgeBall
     this.hasPaid = hasPaid
     this.isHealthy = isHealthy
     this.yearsExperience = yearsExperience
   }
-}
-class DodgeFathersTeammate extends Player {
-  constructor(
-    canThrowBall,
-    canDodgeBall,
-    hasPaid,
-    isHealthy,
-    yearsExperience,
-    mascot,
-    teamColor
-  ) {
-    super(canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience)
-    this.mascot = mascot
-    this.teamColor = teamColor
-  }
-}
-class BallarinasTeammate extends Player {
-  constructor(
-    canThrowBall,
-    canDodgeBall,
-    hasPaid,
-    isHealthy,
-    yearsExperience,
-    mascot,
-    teamColor
-  ) {
-    super(canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience)
-    this.mascot = mascot
-    this.teamColor = teamColor
+
+  // eslint-disable-next-line class-methods-use-this
+  addPlayer() {
+    const playersList = document.getElementById("players")
+    const li = document.createElement("li")
+    const applicantName = document.createElement("h4")
+    const playerId = this.id
+    const playerName = this.name
+    applicantName.appendChild(
+      document.createTextNode(`${playerName} - ${playerId}`)
+    )
+
+    const dodgeFathersbtn = document.createElement("button")
+    dodgeFathersbtn.innerHTML = "Dodge Fathers"
+    dodgeFathersbtn.addEventListener("click", function () {
+      makeTeamMate("dodgeFathers", playerId, playerName)
+      deleteApplicant(event)
+    })
+
+    const ballarinasBtn = document.createElement("button")
+    ballarinasBtn.innerHTML = "Ballarinas"
+    ballarinasBtn.addEventListener("click", function () {
+      makeTeamMate("ballarinas", playerId, playerName)
+      deleteApplicant(event)
+    })
+
+    playersList.append(li)
+    li.appendChild(applicantName)
+    li.appendChild(dodgeFathersbtn)
+    li.appendChild(ballarinasBtn)
   }
 }
 
+// Extend the mascot and colors to players once chosen on a team
+class TeamMate extends Player {
+  constructor(
+    name,
+    mascot,
+    teamColor,
+    id,
+    teamName,
+    canThrowBall,
+    canDodgeBall,
+    hasPaid,
+    isHealthy,
+    yearsExperience
+  ) {
+    super(
+      name,
+      id,
+      canThrowBall,
+      canDodgeBall,
+      hasPaid,
+      isHealthy,
+      yearsExperience
+    )
+    this.mascot = mascot
+    this.teamColor = teamColor
+    this.teamName = teamName
+  }
+
+  addTeamMate() {
+    // TODO:: ask Clayton how to clean up "this" so properties aren't mixed up & can I remove undefined properties
+    console.log(this)
+    const playerName = this.name
+    const playerId = this.id
+    const { teamName } = this
+    const ul = document.getElementById(teamName)
+    const teamMateInfo = `<li class="applicant-entry">
+      <h4 class="applicant-name">${playerName} - <span class="applicant-age"> ${playerId}</span>
+      <span class="applicant-age"> ${this.mascot}</span><span class="applicant-age"> ${this.teamColor}</span>
+      </h4>`
+    ul.insertAdjacentHTML("afterbegin", teamMateInfo)
+  }
+}
+
+const deleteApplicant = (event) => {
+  const applicant = event.target.parentNode
+  const listOfApplicants = applicant.parentNode
+  listOfApplicants.removeChild(applicant)
+}
+
+const makePlayer = (name, id) => {
+  // const [applt] = arrOfApplicants.filter((applicant) => applicant.id === id)
+  const player = new Player(name, id).addPlayer()
+  listOfPlayers.push(player)
+}
+
+// Function that changes the applicants to a player
 const listApplicants = (arr) => {
   const allApplicants = this.document.getElementById("applicants")
   arr.map((applicant) => {
@@ -120,38 +208,18 @@ const listApplicants = (arr) => {
     const button = this.document.createElement("button")
     button.innerHTML = "Make Player"
     button.addEventListener("click", function (event) {
-      makePlayer(applicant.id, event)
+      makePlayer(applicant.name, applicant.id)
       deleteApplicant(event)
     })
-
     li.appendChild(button)
     li.appendChild(
-      this.document.createTextNode(`${applicant.name} - ${applicant.age}`)
+      this.document.createTextNode(`${applicant.name} - ${applicant.id}`)
     )
     allApplicants.append(li)
   })
 }
-const deleteApplicant = (event) => {
-  const applicant = event.target.parentNode
-  const listOfApplicants = applicant.parentNode
-  listOfApplicants.removeChild(applicant)
-}
-
-const makePlayer = (id) => {
-  // listOfPlayers[]
-  // onclick, find applicant in applicantArray & add them to listOfPlayers Array
-  const [player] = arrOfApplicants.filter((applicant) => applicant.id === id)
-
-  // listOfPlayers.push(id)
-  listOfPlayers.push(player)
-  const playersList = this.document.getElementById("players")
-
-  const applicantInfo = `<li class="applicant-entry">
-    <h4 class="applicant-name">${player.name} - <span class="applicant-age">age ${player.age}</span></h4>
-    </li>`
-  playersList.insertAdjacentHTML("afterbegin", applicantInfo)
-
-  // alert(`li ${id} was clicked!`)
-}
 
 // module.exports = { Player, BallarinasTeammate, DodgeFathersTeammate }
+window.onload = function () {
+  listApplicants(arrOfApplicants)
+}
